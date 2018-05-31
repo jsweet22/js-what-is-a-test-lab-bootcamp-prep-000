9e9b6c78659efecd2b00dbd42eea80c4509ab796
Janae Sweet <janaemarie_@hotmail.com>
2018-05-31 20:53:51 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -10,7 +10,7 @@ describe('what-is-a-test', () => {
 
   describe('Height', () => {
     it('is less than 75', () => {
-      expect(index.height).toBeLessThan(40)
+      expect(index.height).toBeLessThan(75)
     })
   })
 
